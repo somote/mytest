983d2e507146b996422d4a9aa8d670e4bfaf546a
Mike Mao <mike@xogrp.com>
2014-12-18 17:38:32 +0800

    refactor code for index and ofc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
+require 'json'
+require 'uri'
 class ApplicationController < ActionController::Base
+  include ConvertHashKeys
+  include Member
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -16,59 +20,96 @@ class ApplicationController < ActionController::Base
     if @isLoggedIn.present?
       @userId = member_id
       response = Api::RegistryApi.get_couples_summary_by_user_id(@userId)
-      process_response(response)
+      process_couples_response(response)
     end
   end
 
-  def about
-    if @isLoggedIn.present?
-      @userId = 1
-      if response.present?
-        @charity = 2
-        @coupleRegistries = {
-            registries:process_registries(1),
-            registries_len:2,
-            stats: {
-                total: 3,
-                fulfilled: 4
-            }
+  def manage
+    gon.ENV = gon_env
+    gon.shortenPrefix= Settings.webui_shorten_root_url
+    # render page as bvr
+    if current_member
+      gon.member = current_member.present? ? camelize_hash(current_member.as_json) : {}
+    else
+      retailers = Api::RegistryApi.get_retailer_to_create_registry
+      gon.retailers = JSON.parse retailers.body
+    end
+
+    @layout_data = TheKnotLayout::Data.new(title: 'Wedding Gift Registry', description: 'example meta description')
+    @leaderboard_ad_hidden = true
+  end
+
+  def guest
+    couple_id = params[:couple_id]
+
+    if /\d+/.match(couple_id)
+      couple = Api::RegistryApi.get_couple couple_id
+
+      begin
+        couple = JSON.parse(couple.body)
+        couple_name = get_couple_name couple
+        event_date = Date.parse(couple['EventDate']).strftime('%B %d, %Y')
+        location = get_location couple
+        retailer_names = get_retailer_names couple['CoupleRegistries']
+
+        @title = couple_name << ' - Wedding Registry'
+        @description = "#{couple['Registrant1FirstName']}#{couple['Registrant2FirstName'].present? ? ' and ' + couple['Registrant2FirstName'] : ''}
+          from #{ location } have registered at #{retailer_names}
+          for their wedding on #{ event_date }.
+          View all of the items from their registries in one beautiful list."
+
+        gon.ENV = gon_env
+        gon.couple_info = {
+            username1: "#{couple['Registrant1FirstName']} #{couple['Registrant1LastName']}",
+            username2: ("#{couple['Registrant2FirstName']} #{couple['Registrant2LastName']}" unless couple['Registrant2FirstName'].nil? and couple['Registrant2LastName'].nil?),
+            eventdate: event_date,
+            coupleid: couple['Id'],
+            location: location,
+            coupleregistries: couple['CoupleRegistries']
         }
+        gon.charity = Api::RegistryApi.fix_charity_url couple['User']['UserCharity']
+        gon.personal_websites = couple['PersonalWebsites'].nil? ? [] : couple['PersonalWebsites'].select { |web| [994,950].include? web['AffiliateId']}
+        gon.isHiddenProducts = couple['IsHiddenProducts']
+        gon.profileURL = {
+            shortUrl: couple['UniversalRegistry'].nil? ? '' : couple['UniversalRegistry']['ShortUrl'],
+            universalRegistryId: couple['UniversalRegistry'].nil? ? '' : couple['UniversalRegistry']['Id'],
+            longUrl: couple['UniversalRegistryLongUrl']
+        }
+
+        render layout: false
+      rescue
+        redirect_to_hub
       end
+    else
+      redirect_to_hub
     end
   end
 
-  def product
-    @layout_data = TheKnotLayout::Data.new({
-                                               title: 'Wedding Registry & Bridal Registries - The Knot',
-                                               description: 'Get tips and trends in wedding registries. Find the top shops to set up your wedding registry and more. Browse through different bridal registries and set up a registry today.'
-                                           })
-    @xo_metadata = XO::Metadata::Builder.new(application_name: 'Registry')
-    @hub = true
-    @isLoggedIn = current_member
-    gon.ENV = gon_env
-
-    if @isLoggedIn.present?
-      @userId = member_id
-      response = Api::RegistryApi.get_couples_summary_by_user_id(@userId)
-    end
-  end
-
-  def book
-    @layout_data = TheKnotLayout::Data.new({})
-    @xo_metadata = XO::Metadata::Builder.new(application_name: 'Registry')
-    @hub = true
-    @isLoggedIn = current_member
-    gon.ENV = gon_env
-
-    if @isLoggedIn.present?
-      @userId = member_id
-      response = Api::RegistryApi.get_couples_summary_by_user_id(@userId)
-    end
+  def couple_search
+    @coupleSearch = true
+    gon.ENV = {
+        proxyRoot: Settings.legacy_webui_root_url,
+        filterLimit: Settings.couple_search_filter_limit,
+        contentProxyUrlRootPath: Settings.content_proxy_root_url
+    }
+    @xo_metadata = XO::Metadata::Builder.new(application_name: 'Registry Couple Search')
+    @layout_data = TheKnotLayout::Data.new(title: 'Registry Couple Search')
+    render template: 'application/couple_search'
   end
 
   private
 
-  def process_response(response)
+  def gon_env
+    {
+        cookieName: Settings.cookie_name,
+        serviceRoot: Settings.webapi_root_url,
+        proxyRoot: Settings.legacy_webui_root_url,
+        webuiUrlRootPath: Settings.webui_url_root_path,
+        contentProxyUrlRootPath: Settings.content_proxy_root_url
+    }
+  end
+
+  def process_couples_response(response)
     if response.present?
       @coupleId = response["CoupleId"]
       @coupleRegistries = {
@@ -81,5 +122,52 @@ class ApplicationController < ActionController::Base
       }
       @charity = response["UserCharity"]
     end
+  end
+
+  def process_registries(registries)
+    temp_registries_array = registries.clone
+    temp_registries_array.delete_if {|registry|!registry["LogoImageUrl"].present?}
+    if temp_registries_array.length == 0
+      registries.slice(0,2)
+    else
+      proxy = Settings.content_proxy_root_url
+      temp_registries_array.slice(0,2).each_with_index { |registry|
+        registry["LogoImageUrl"]=proxy + URI.escape(registry["LogoImageUrl"], Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+      }
+    end
+  end
+
+  def get_retailer_names(registries)
+    retailer_names = Array.new(registries).delete_if {|registry| registry['Retailer'].nil?}
+    retailer_names = retailer_names.map {|registry| registry['Retailer']['Name']}
+
+    if retailer_names.length < 2
+      retailer_names.to_s
+    else
+      retailer_names.take(retailer_names.count - 1).join(', ') << ' and ' << retailer_names.at(retailer_names.count - 1)
+    end
+  end
+
+  def get_couple_name(couple, conn = '&')
+    space = ' '
+    first = couple['Registrant1FirstName'] + space + couple['Registrant1LastName']
+    second = couple['Registrant2FirstName'] +
+        (couple['Registrant2LastName'].present? ? space + couple['Registrant2LastName'] : '')
+
+    first << (second.present? ? (space + conn + space + second) : '')
+  end
+
+  def get_location(couple)
+    if /usa?/i.match couple['Country']
+      location = "#{couple['City']}, #{couple['State']}"
+    else
+      location = "#{couple['City']}, #{couple['CountryFullName']}"
+    end
+
+    /^[\s,]+$/.match(location) ? '' : location.gsub(/^,\s|,\s$/, '')
+  end
+
+  def redirect_to_hub
+    redirect_to ''
   end
 end
--- app/controllers/ofc_controller.rb
+++ app/controllers/ofc_controller.rb
@@ -6,77 +6,85 @@ class Api::OfcController < ApiController
 
   def create
     begin
-      init
-      render_fail('Params is invalid') and return if invalid
+      init_registry_request
+      valid_registry_request
       clear_token(params['UserId'], params['RetailerId']) if params['Retry'].nil?
-      token = try_get_token()
-      render_fail('Authorization code is invalid') and return if token.nil?
-      r = retailer_registry(token)
-      render json: r and return if @rid.nil?
-      tk_registry(rid, @tk_registry_request)
+      token = get_token
+      retailer_registry = process_retailer_registry(token)
+      render json: retailer_registry and return if @rid.nil?
+      process_tk_registry
     rescue Exception => e
-      render_fail(e.message)
+      render json: {status: :failed, msg: e.message}
     end
   end
 
   def create_item
     begin
-      init
-      render_fail('Params is invalid') and return if invalid
-      token = try_get_token()
-      render_fail('Authorization code is invalid') and return if token.nil?
-      item = service.create_registry_item(params, token)
+      init_service
+      valid_registry_item_request
+      token = get_token
+      item = @service.create_registry_item(params, token)
       render json: item
     rescue Exception => e
-      render_fail(e.message)
+      render json: {status: :failed, msg: e.message}
     end
   end
 
   private
 
-  def init
-    @service = Api::OfcRetailerServiceProvider.get_service(params['RetailerId'])
+  def init_registry_request
+    init_service
     @retailer_registry_request = RetailerRegistryRequest.new(params)
     @tk_registry_request = TKRegistryRequest.new(params)
   end
 
-  def try_get_token
-    token = get_token(params['Code'], params['UserId'], params['RetailerId'])
+  def init_service
+    @service = Api::OfcRetailerServiceProvider.get_service(params['RetailerId'])
   end
 
-  def invalid
-    return false if params['UserId'].nil? or params['RetailerId'].nil? or @service.nil?
-    retailer_registry_request.valid? and tk_registry_request.valid?
+  def valid_registry_request
+    valid_base
+    raise 'Params is invalid' if
+        not @retailer_registry_request.valid? or
+            not @tk_registry_request.valid?
   end
 
-  def retailer_registry(token)
+  def valid_registry_item_request
+    valid_base
+    raise 'Params is invalid' if
+        params['sku'].nil? or
+            params['quantity'].nil?
+  end
+
+  def valid_base
+    raise 'Params is invalid' if
+        params['UserId'].nil? or
+            params['RetailerId'].nil?
+    raise 'UnKnow Retailer' if @service.nil?
+  end
+
+  def process_retailer_registry(token)
     if params['Retry'].nil?
       retailer_registry = @service.create_retailer_registry(@retailer_registry_request, token)
       @rid = retailer_registry['registryId']
     else
       @rid = params['RetailerRegistryId']
     end
+    @tk_registry_request.set_code @rid unless @rid.nil?
     retailer_registry
   end
 
-  def tk_registry(rid, tk_registry_request)
-    @tk_registry_request.set_code rid
-    tk_registry = Api::RegistryApi.upsert_retailer_registry(params['UserId'],tk_registry_request.to_json)
-    if tk_registry.status != 200
-      render json: {status: :failed, msg: tk_registry.body, RetailerRegistryId: rid}
+  def process_tk_registry
+    tk_registry = Api::RegistryApi.upsert_retailer_registry(params['UserId'],@tk_registry_request.to_json)
+    if tk_registry.status == 200
+      render json: JSON.parse(tk_registry.body)
+    else
+      render json: {status: :failed, msg: tk_registry.body, RetailerRegistryId: @rid}
     end
-    render json: JSON.parse(tk_registry.body)
   end
 
-  def invalid_item
-    params['UserId'].nil? or params['RetailerId'].nil? or params['sku'].nil? or params['quantity'].nil?
-  end
-
-  def render_fail(msg)
-    render json: {status: :failed, msg: msg}
-  end
-
-  def get_token(code, user_id, retailer_id)
+  def get_token
+    code, user_id, retailer_id, service = params['Code'], params['UserId'], params['RetailerId']
     token = get_token_from_cookies(user_id, retailer_id)
     return token if not token.nil? and not token == ''
 
@@ -91,7 +99,7 @@ class Api::OfcController < ApiController
 
   def get_retailer_token(code, is_refresh, user_id, retailer_id)
     token = is_refresh ? @service.refresh_token(code) : @service.get_token(code)
-    return nil if token.nil? or token['access_token'].nil?
+    raise 'Authorization code is invalid' if token.nil? or token['access_token'].nil?
 
     store_token(token['access_token'], user_id, retailer_id)
     store_refresh_token(token['refresh_token'], user_id, retailer_id)
