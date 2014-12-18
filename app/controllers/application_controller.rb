589bb95e0a0891309ad867d964f19f04c3d45a40
Mike Mao <mike@xogrp.com>
2014-12-18 12:43:00 +0800

    update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,4 +30,58 @@ class ApplicationController < ActionController::Base
       end
     end
   end
+
+  def about
+    @layout_data = TheKnotLayout::Data.new({})
+    @xo_metadata = XO::Metadata::Builder.new(application_name: 'Registry')
+    @hub = true
+    @isLoggedIn = current_member
+    gon.ENV = gon_env
+
+    if @isLoggedIn.present?
+      @userId = member_id
+      response = Api::RegistryApi.get_couples_summary_by_user_id(@userId)
+      if response.present?
+        @coupleId = response["CoupleId"]
+        @coupleRegistries = {
+            registries:process_registries(response["CoupleRegistries"]),
+            registries_len:response["NumberOfCoupleRegistries"],
+            stats: {
+                total: response["NumberOfProducts"],
+                fulfilled: response["NumberOfProducts"] - response["NumberOfUnfulfilledProducts"]
+            }
+        }
+        @charity = response["UserCharity"]
+      end
+    end
+  end
+
+  def product
+    @layout_data = TheKnotLayout::Data.new({
+                                               title: 'Wedding Registry & Bridal Registries - The Knot',
+                                               description: 'Get tips and trends in wedding registries. Find the top shops to set up your wedding registry and more. Browse through different bridal registries and set up a registry today.'
+                                           })
+    @xo_metadata = XO::Metadata::Builder.new(application_name: 'Registry')
+    @hub = true
+    @isLoggedIn = current_member
+    gon.ENV = gon_env
+
+    if @isLoggedIn.present?
+      @userId = member_id
+      response = Api::RegistryApi.get_couples_summary_by_user_id(@userId)
+    end
+  end
+
+  def product
+    @layout_data = TheKnotLayout::Data.new({})
+    @xo_metadata = XO::Metadata::Builder.new(application_name: 'Registry')
+    @hub = true
+    @isLoggedIn = current_member
+    gon.ENV = gon_env
+
+    if @isLoggedIn.present?
+      @userId = member_id
+      response = Api::RegistryApi.get_couples_summary_by_user_id(@userId)
+    end
+  end
 end
